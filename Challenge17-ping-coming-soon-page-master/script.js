2a411f90a6c4e4e777e72c02088753c546d3c7c7
Daniel Souza <danielsouzaaj@hotmail.com>
2024-07-19 01:57:53 -0300

    Updated js

--- Challenge17-ping-coming-soon-page-master/script.js
+++ Challenge17-ping-coming-soon-page-master/script.js
@@ -3,18 +3,22 @@ const input = document.querySelector('[type="email"]');
 const button = document.querySelector(".btn");
 
 form.addEventListener("submit", checkEmail);
-input.addEventListener("click", removeInvalidMsg);
+input.addEventListener("keyup", removeInvalidMsg);
 
 function checkEmail(event) {
   event.preventDefault();
+
   if (!validateEmail(input.value) && !input.classList.contains("invalid")) {
     input.classList.add("invalid");
     form.insertBefore(
       invalidMsg("Please provide a valid email address"),
       button
     );
-  }else {
-    input.value = ""
+    return;
+  }
+
+  if (validateEmail(input.value)) {
+    input.value = "";
   }
 }
 
@@ -26,8 +30,8 @@ function validateEmail(email) {
     );
 }
 
-function removeInvalidMsg() {
-  if (input.classList.contains("invalid")) {
+function removeInvalidMsg(event) {
+  if (input.classList.contains("invalid") && event.keyCode !== 13) {
     const invalidMsg = document.querySelector(".invalid-text");
     input.classList.remove("invalid");
     invalidMsg.remove();
