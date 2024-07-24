856c5ffdf563f9417c8fa581a1cfe8aaee312b26
Daniel Souza <danielsouzaaj@hotmail.com>
2024-07-24 02:44:03 -0300

    Updated js

--- Challenge20-faq-accordion-main/index.js
+++ Challenge20-faq-accordion-main/index.js
@@ -1,15 +1,28 @@
-const accordion = document.querySelectorAll('.accordion')
+const accordion = document.querySelectorAll(".accordion");
+let accordionActive;
 
-accordion.forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('active')
+accordion.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (
+      accordionActive &&
+      accordionActive !== item &&
+      accordionActive.classList.contains("active")
+    ) {
+      accordionActive.classList.remove("active");
+      let panelActive = accordionActive.nextElementSibling;
+      panelActive.style.display = "none";
+    }
 
-        let panel = item.nextElementSibling
-        
-        if (panel.style.display === 'block') {
-            panel.style.display = 'none'
-        }else {
-            panel.style.display = 'block'
-        }
-    })
-})
+    item.classList.toggle("active");
+
+    let panel = item.nextElementSibling;
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+
+    accordionActive = item;
+  });
+});
