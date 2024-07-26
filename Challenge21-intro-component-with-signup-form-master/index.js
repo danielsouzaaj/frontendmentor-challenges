5fcd14f77932cac4f40554ef9d2d795f1b98467a
Daniel Souza <danielsouzaaj@hotmail.com>
2024-07-25 23:37:42 -0300

    Updated js

--- Challenge21-intro-component-with-signup-form-master/index.js
+++ Challenge21-intro-component-with-signup-form-master/index.js
@@ -8,38 +8,36 @@ form.addEventListener("submit", (event) => {
   campos.forEach((item, index) => {
     if (item.value === "") {
       item.setCustomValidity(`${item.placeholder} cannot be empty`);
-    }
-
-    if (
+    } else if (
       item.type === "email" &&
       item.value !== "" &&
       !validateEmail(item.value)
     ) {
       item.setCustomValidity("Looks like this is not an email");
+    } else {
+      item.setCustomValidity("");
     }
 
     if (item.validationMessage && !item.classList.contains("erro")) {
-      formControl[index].insertBefore(addError(item.validationMessage), item);
+      formControl[index].insertBefore(textError(item.validationMessage), item);
       item.classList.add("erro");
     }
   });
+
+  clearValue();
 });
 
 campos.forEach((item, index) => {
   item.addEventListener("focus", () => {
-    if (item.classList.contains("erro")) {
-      item.classList.remove("erro");
-      formControl[index].querySelector(".text-erro").remove();
-    }
+    removeError(item, index);
   });
 });
 
-function addError(text) {
-  let textElement = document.createElement("p");
-  textElement.classList.add("text-erro");
-  textElement.innerText = text;
-
-  return textElement;
+function removeError(item, index) {
+  if (item.classList.contains("erro")) {
+    item.classList.remove("erro");
+    formControl[index].querySelector(".text-erro").remove();
+  }
 }
 
 function validateEmail(email) {
@@ -48,4 +46,20 @@ function validateEmail(email) {
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
+}
+
+function textError(text) {
+  let textElement = document.createElement("p");
+  textElement.classList.add("text-erro");
+  textElement.innerText = text;
+
+  return textElement;
+}
+
+function clearValue() {
+  let check = [...campos].find((item) => item.validationMessage !== "");
+
+  if (!check) {
+    campos.forEach((item) => (item.value = ""));
+  }
 }
