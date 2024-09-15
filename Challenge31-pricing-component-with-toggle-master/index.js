6da3249c227911bde152d29afe76ca3b54f4066e
Daniel Souza <danielsouzaaj@hotmail.com>
2024-09-15 12:54:39 -0300

    Updated js

--- Challenge31-pricing-component-with-toggle-master/index.js
+++ Challenge31-pricing-component-with-toggle-master/index.js
@@ -5,10 +5,10 @@ input.addEventListener('change', (e) => {
     const annually = document.querySelectorAll('.annually')
     
     if(e.target.checked) {
-        monthly.forEach(item => item.style.display = 'block')
+        monthly.forEach(item => item.style.display = 'flex')
         annually.forEach(item => item.style.display = 'none')
     }else {
         monthly.forEach(item => item.style.display = 'none')
-        annually.forEach(item => item.style.display = 'block')
+        annually.forEach(item => item.style.display = 'flex')
     }
 })
